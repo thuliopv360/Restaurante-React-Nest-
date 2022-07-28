0818cb528f53714459d82e0abdda1d0a884df36e
Thulio Paiva <thuliopv360@gmail.com>
2022-07-27 22:01:51 -0300

    adiction the assets

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -43,9 +43,37 @@ const Home = () => {
         </section>
       </div>
       <aside>
-        <div></div>
-        <div></div>
-        <div></div>
+        <header>
+          <h2>Pedido 12</h2>
+          <div>
+            <button>Comer No Local</button>
+            <button>Viagem</button>
+            <button>Delivery</button>
+          </div>
+        </header>
+        <div>
+          <div>
+            <h3>Item</h3>
+            <h3>Qtd</h3>
+            <h3>Preço</h3>
+          </div>
+          <div className="checkout-cards-container">
+            <div>Card Checkout</div>
+            <div>Card Checkout</div>
+            <div>Card Checkout</div>
+          </div>
+        </div>
+        <div>
+          <div>
+            <p>Desconto: </p>
+            <p>R$ 0,00</p>
+          </div>
+          <div>
+            <p>Sub Total</p>
+            <p>R$ 0,00</p>
+          </div>
+          <button>Continuar para o pagamento</button>
+        </div>
       </aside>
     </div>
   );
