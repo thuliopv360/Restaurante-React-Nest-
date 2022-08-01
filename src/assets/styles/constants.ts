d196270f81e9c617a20d2f9a45073ebf3c5c8597
Thulio Paiva <thuliopv360@gmail.com>
2022-08-01 20:13:30 -0300

    feature: creation the constants theme

---
+++ src/assets/styles/constants.ts
@@ -0,0 +1,9 @@
+export const constants = {
+  logoFontFamily: "Big Shoulders Inline Display",
+  bodyFontFamily: "Barlow",
+
+  bodyFontSize: "14px",
+  logoFontSize: "28px",
+  headingFontSize: "28px",
+  LineHeight: "140%",
+};
--- src/assets/styles/theme.ts
+++ src/assets/styles/theme.ts
@@ -1,7 +1,9 @@
+import { constants } from "./constants";
 import { colors } from "./colors";
 
 const theme = {
   colors,
+  constants,
 } as const;
 
 export default theme;
