2f897b61b7118e603f54c67fde8fc7fe5afefd13
Thulio Paiva <thuliopv360@gmail.com>
2022-08-01 21:52:39 -0300

    feature: creation the orderDetails

---
+++ src/components/OrderDetails/index.tsx
@@ -0,0 +1,41 @@
+import * as Styled from "./styles";
+
+const OrderDetails = () => {
+  return (
+    <Styled.OrderDetailsContainer>
+      <header>
+        <h2>Pedido #12</h2>
+        <div>
+          <button>Comer No Local</button>
+          <button>Viagem</button>
+          <button>Delivery</button>
+        </div>
+      </header>
+      <div>
+        <div>
+          <h3>Item</h3>
+          <h3>Qtd</h3>
+          <h3>Preço</h3>
+        </div>
+        <div className="checkout-cards-container">
+          <div>Card Checkout</div>
+          <div>Card Checkout</div>
+          <div>Card Checkout</div>
+        </div>
+      </div>
+      <div>
+        <div>
+          <p>Desconto: </p>
+          <p>R$ 0,00</p>
+        </div>
+        <div>
+          <p>Sub Total</p>
+          <p>R$ 0,00</p>
+        </div>
+        <button>Continuar para o pagamento</button>
+      </div>
+    </Styled.OrderDetailsContainer>
+  );
+};
+
+export default OrderDetails;
