1e6e5ddce8e40896b093d4f641b460e354ad8937
Thulio Paiva <thuliopv360@gmail.com>
2022-08-08 14:33:18 -0300

    feature: creation the login pages and logo images e background images in login

---
+++ src/components/Admin/index.tsx
@@ -0,0 +1,13 @@
+import * as Styled from "./styles";
+
+const Admin = () => {
+  return (
+    <div>
+      <div>
+        <h1>Hello</h1>
+      </div>
+    </div>
+  );
+};
+
+export default Admin;
--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,16 +1,64 @@
 import Input from "../../components/Input";
 import * as Styled from "./styles";
+import Button from "../../components/Button";
+import { Dispatch, SetStateAction, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { toast } from "react-hot-toast";
+import Logo from "../../assets/logo_patterns/Logo.png";
+import { mockedUsers } from "../../mocks";
 
-const Login = () => {
+interface LoginProps {
+  setLogged: Dispatch<SetStateAction<boolean>>;
+}
+
+const Login = ({ setLogged }: LoginProps) => {
+  const navigate = useNavigate();
+
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+
+  const handleLogin = () => {
+    if (
+      email === mockedUsers[0].email &&
+      password === mockedUsers[0].password
+    ) {
+      setLogged(true);
+      navigate("/");
+      toast.success("Login bem sucedido!");
+      return;
+    } else if (email === "admin" && password === "admin") {
+      setLogged(true);
+      navigate("/");
+      toast.success("Login bem sucedido!");
+      return;
+    } else if (email === "Thulio@gmail.com" && password === "123@bc") {
+      setLogged(true);
+      navigate("/admin");
+      toast.success("Pagina de admin");
+      return;
+    }
+    toast.error("Usuário ou senha incorretos.");
+  };
+
   return (
     <Styled.LoginPageContainer>
       <Styled.LoginFormContainer>
-        <div>
+        <Styled.LoginLogoContainer>
           <h1>Restaurante Fresh</h1>
-          <img src="" alt="" />
-        </div>
-        <Input />
-        <Input />
+          <img alt="logo" src={Logo} />
+        </Styled.LoginLogoContainer>
+        <Input
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          placeholder="Email"
+        />
+        <Input
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          placeholder="Senha"
+        />
+        <Button text="Entrar" size="large" onClick={handleLogin} />
       </Styled.LoginFormContainer>
     </Styled.LoginPageContainer>
   );
