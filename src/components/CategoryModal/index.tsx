6ae4aceab59e60b6318b02ea7c1d232da731e3bd
Thulio Paiva <thuliopv360@gmail.com>
2022-08-19 17:37:12 -0300

    feature: finished the new category modal

--- src/components/CategoryModal/index.tsx
+++ src/components/CategoryModal/index.tsx
@@ -1,21 +1,80 @@
 import Button from "../Button";
-import { ModalOverlay, StyledInput } from "../../assets/styles/globalStyles";
+import {
+  ErrorMessage,
+  ModalOverlay,
+  StyledInput,
+} from "../../assets/styles/globalStyles";
 import { ModalContainer } from "./styles";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { useForm } from "react-hook-form";
+import * as yup from "yup";
+import { api } from "../../services";
+import toast from "react-hot-toast";
+import { useCategories } from "../../contexts/categories";
 
 interface CategoryModalProps {
   handleOpenModal: () => void;
 }
 
+interface NewCategoryData {
+  name: string;
+}
+
+const newCategorySchema = yup.object().shape({
+  name: yup.string().required("Obrigadorio imformar nome das categorias"),
+});
+
+const updateCategorySchema = yup.object().shape({
+  name: yup.string().required("Obrigadorio imformar nome das categorias"),
+});
+
 const CategoryModal = ({ handleOpenModal }: CategoryModalProps) => {
+  const { handleGetCategories } = useCategories();
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<NewCategoryData>({
+    resolver: yupResolver(newCategorySchema),
+  });
+
+  const handleNewCategory = (data: NewCategoryData) => {
+    const token = localStorage.getItem("token");
+
+    const headers = {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    };
+
+    api
+      .post("/categories", data, headers)
+      .then(() => {
+        toast.success("Categoria criada com sucesso");
+        handleGetCategories();
+        handleOpenModal();
+      })
+      .catch(() => {
+        toast.error("Erro na hora de criar categoria");
+      });
+  };
+
   return (
     <ModalOverlay>
-      <ModalContainer>
+      <ModalContainer onSubmit={handleSubmit(handleNewCategory)}>
         <h2>Criar nova categoria</h2>
-        <StyledInput placeholder="Nome da categoria" />
+        <StyledInput placeholder="Nome da categoria" {...register("name")} />
         <div>
-          <Button text="Cancelar" variant="cancel" onClick={handleOpenModal} />
-          <Button text="Enviar" />
+          <Button
+            text="Cancelar"
+            variant="cancel"
+            onClick={handleOpenModal}
+            size="small"
+          />
+          <Button text="Enviar" type="submit" size="small" />
         </div>
+        <ErrorMessage>{errors.name?.message}</ErrorMessage>
       </ModalContainer>
     </ModalOverlay>
   );
