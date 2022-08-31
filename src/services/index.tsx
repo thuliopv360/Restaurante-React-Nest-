ce715b86f952f6ec165e160cc92ba10407a9e6aa
unknown <thuliopv360@gmail.com>
2022-08-31 00:07:36 -0300

    feature: fix error in the categories products

--- src/services/index.tsx
+++ src/services/index.tsx
@@ -2,6 +2,5 @@ import axios from "axios";
 
 export const api = axios.create({
   baseURL: "https://api-restaurante.onrender.com",
+  // baseURL: "http://localhost:3333",
 });
-
-// baseURL: "http://localhost:3333",
